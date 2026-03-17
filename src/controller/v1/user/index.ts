caf3ee90a55616c62f5f3ee789cc00f5b0a03c2c
Apoorv Saxena <apoorv.saxena@synexc.com>
2026-03-17 19:10:44 +0530

    [Add]
    add send-otp route

--- src/controller/v1/user/index.ts
+++ src/controller/v1/user/index.ts
@@ -1,10 +1,28 @@
+import dayjs from 'dayjs';
+import { OTP_STATUS } from '../../../constant';
 import { IRequest, IResponse, makeResponse } from '../../../lib';
-import { createUser, getUser } from '../../../services';
+import { createUser, getOtp, getUser } from '../../../services';
 
 const signupHandler = async (req: IRequest, res: IResponse) => {
   const body = req.body;
 
-  const existing = await getUser({ 'contact.email': body.contact.email });
+  let contact = '';
+  if (body.contact?.email) {
+    contact = body.contact?.email;
+    body.contact.isEmailVerified = true;
+  } else {
+    contact = `${body.contact?.mobile?.dailcode}${body.contact?.mobile?.number}`;
+  }
+  const record = await getOtp({
+    contact,
+    status: OTP_STATUS.verified,
+  });
+
+  if (!record || dayjs().isAfter(dayjs(record.expiresAt))) {
+    return makeResponse(req, res, 400, false, 'otp_expired');
+  }
+
+  const existing = await getUser({ 'contact.email': body.contact?.email });
   if (existing) return makeResponse(req, res, 400, false, 'email_exit');
 
   await createUser(body);
