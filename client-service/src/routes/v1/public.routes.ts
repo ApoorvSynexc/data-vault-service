e9a7a0fa77b699dbd4b2a778455fd33041e14036
tanishak-jangir <tanishak.jangir@synexc.com>
2026-04-20 16:14:14 +0530

    Revert "Add Token Introspection"
    This reverts commit 2f6d529fa5b5a0c68773cf86c76f2191e84d62d3.

--- client-service/src/routes/v1/public.routes.ts
+++ client-service/src/routes/v1/public.routes.ts
@@ -4,6 +4,6 @@ import { webhookAuth } from '../../middlewares';
 
 const router = Router();
 
-router.put('/webhook/salesforce', webhookAuth, publicController.salesForceRealTimeHandler);
+router.put('/webhook/salesforce', webhookAuth, publicController.salesForceealTimeHandler);
 
 export const publicRouter = router;
