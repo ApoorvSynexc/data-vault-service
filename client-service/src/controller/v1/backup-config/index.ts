85398ecbfa19c589d3c3f210b905f0e3980a2613
Apoorv Saxena <apoorv.saxena@synexc.com>
2026-05-15 17:48:51 +0530

    [Update]
    update backup triggers

--- client-service/src/controller/v1/backup-config/index.ts
+++ client-service/src/controller/v1/backup-config/index.ts
@@ -128,7 +128,7 @@ const createBackupConfigHandler = async (req: IRequest, res: IResponse): Promise
     }
 
     if (config.schedule === SCHEDULE_MODE.realtime) {
-      // await triggerBackupJob(config);
+      await triggerBackupJob(config);
     } else if (config.schedule === SCHEDULE_MODE.schedule && config.scheduleConfig) {
       const scheduleConfig = req.body.scheduleConfig;
       const isOnceImmediate = scheduleConfig?.scheduling?.frequency === 'ONCE'
