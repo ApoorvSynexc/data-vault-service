114e65e632404a2cc420bc9e4ca37440cbaab5be
Apoorv Saxena <apoorv.saxena@synexc.com>
2026-05-19 14:39:11 +0530

    [Update]
    update looger in backup config

--- client-service/src/middlewares/webhook-auth/index.ts
+++ client-service/src/middlewares/webhook-auth/index.ts
@@ -5,6 +5,8 @@ import { getBackupConfigById } from '../../services/backup-config';
 export const webhookAuth = async (req: Request, res: Response, next: NextFunction) => {
   const backupConfigId = req.headers['x-webhook-secret'];
 
+  console.log({"x-webhook-secret": backupConfigId});
+  
   if(!backupConfigId || typeof backupConfigId !== 'string') {
     return makeResponse(req, res, 401, false, 'unauthorized');
   }
