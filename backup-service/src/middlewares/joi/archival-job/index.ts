27c1bba1ce5c80e18974b4aed9b41a815f6c21ff
Apoorv Saxena <apoorv.saxena@synexc.com>
2026-06-02 18:41:46 +0530

    [Update]
    update archical backup

--- backup-service/src/middlewares/joi/archival-job/index.ts
+++ backup-service/src/middlewares/joi/archival-job/index.ts
@@ -34,6 +34,7 @@ const conditionSchema = Joi.object({
 const objectSchema = Joi.object({
     id: Joi.string().required(),
     name: Joi.string().required(),
+    fieldApiName: Joi.string().optional(),
     condition: conditionSchema.optional(),
     type: Joi.string().optional(),
     field: Joi.array().items(objectFieldSchema).required(),
