514226c43d156767ae06f55aa46c8528fe7a29dd
Apoorv Saxena <apoorv.saxena@synexc.com>
2026-06-08 15:20:39 +0530

    [Add]
    update stats api

--- client-service/src/controller/v1/auth/social-login.ts
+++ client-service/src/controller/v1/auth/social-login.ts
@@ -36,7 +36,7 @@ const parseSalesforceError = (error: any): string | null => {
 };
 
 const socialLoginHandler = async (req: IRequest, res: IResponse): Promise<void> => {
-  const { authProvider, environment }  = req.query as {authProvider: string,environment: SalesforceEnvironment };
+  const { authProvider, environment, customUrl }  = req.query as {authProvider: string,environment: SalesforceEnvironment, customUrl: string };
 
   if (!authProvider) {
     makeResponse(req, res, 400, false, 'auth_provider_required');
@@ -49,7 +49,7 @@ const socialLoginHandler = async (req: IRequest, res: IResponse): Promise<void> 
 
   switch (authProviderStr) {
     case 'salesforce': {
-      const { url, codeVerifier, state } = getSalesforceLoginUrl(undefined, SALESFORCE_LOGIN_REDIRECT_URI, environment);
+      const { url, codeVerifier, state } = getSalesforceLoginUrl(undefined, SALESFORCE_LOGIN_REDIRECT_URI, environment, customUrl);
       await createOAuthState(state, codeVerifier, '', authProviderStr);
       authorizationUrl = url;
       break;
