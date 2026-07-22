032d80c72e09b087a001d53539ec1c744c2a0454
Apoorv Saxena <apoorv.saxena@synexc.com>
2026-07-22 10:51:40 +0530

    [Add]
    restore schema changes

--- backup-service/src/controller/v1/restore/index.ts
+++ backup-service/src/controller/v1/restore/index.ts
@@ -1,11 +1,20 @@
 import { IRequest, IResponse, makeResponse } from "../../../lib";
+import { getRestoreJobById } from "../../../services";
 import { wrapController } from "../../../utils/helper";
 
 const createRestoreJobHandler = async (req: IRequest, res: IResponse): Promise<void> => {
-  const { restoreId } = req.query as { restoreId: string };
-  
+  const { restoreJobId,  } = req.query as { restoreJobId: string };
 
-  makeResponse(req, res, 200, true, 'create');
+  const restoreJob = await getRestoreJobById(restoreJobId);
+  if (!restoreJob) {
+    return makeResponse(req, res, 400, false, 'not_exist');
+  }
+
+  if (restoreJob.status !== 'pending') {
+    return makeResponse(req, res, 400, false, 'not_exist');
+  }
+
+  return makeResponse(req, res, 200, true, 'create');
 };
 
 export const createRestoreController = wrapController({
