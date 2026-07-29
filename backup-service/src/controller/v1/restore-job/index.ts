4ffc670a46b64a6417b3681b284040952b201a2d
Apoorv Saxena <apoorv.saxena@synexc.com>
2026-07-30 00:06:34 +0530

    [update]
    restore to update joi validation

--- backup-service/src/controller/v1/restore-job/index.ts
+++ backup-service/src/controller/v1/restore-job/index.ts
@@ -6,18 +6,21 @@ import { wrapController } from '../../../utils/helper';
 const createRestoreJobHandler = async (req: IRequest, res: IResponse): Promise<void> => {
   const { restoreJobId } = req.body as { restoreJobId: string };
 
-  console.log('createRestoreJobHandler', JSON.stringify({ body: req.body }));
+  console.log('1111111111111createRestoreJobHandler', JSON.stringify({ body: req.body }));
   const restoreJob = await getRestoreJobById(restoreJobId);
   if (!restoreJob) {
     return makeResponse(req, res, 400, false, 'not_exist');
   }
 
+  console.log("22222222222");
   if (restoreJob.status !== 'PENDING') {
     return makeResponse(req, res, 400, false, 'not_exist');
   }
 
+  console.log("333333333333");
   makeResponse(req, res, 200, true, 'create');
   runRestoreJob(restoreJob).catch(() => {});
+  console.log("444444444444");
 };
 
 export const restoreController = wrapController({
