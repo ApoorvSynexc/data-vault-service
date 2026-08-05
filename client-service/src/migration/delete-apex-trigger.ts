4d6042c18d25bff5b6fcf77d0de0b60ac92fefec
Apoorv Saxena <apoorv.saxena@synexc.com>
2026-08-05 12:52:02 +0530

    [Update]
    update aws credentials

---
+++ client-service/src/migration/delete-apex-trigger.ts
@@ -0,0 +1,32 @@
+import { deleteTriggers, getUserByCrmProfileUserId } from "../services"
+import { decrypt } from "../utils/encryption";
+
+export const deleteUserTrigger = async () => {
+    try {
+        const backupConfig: any = {
+            triggerResults: [
+                {
+                    triggerName: "DataVault__Account_Trigger"
+                }
+            ]
+        }
+        const user = await getUserByCrmProfileUserId("005dN00000B4141QAB");
+        if (user && user.crmCredential && user.crmProfile?.instanceUrl) {
+            console.log("Deleting trigger");
+
+            const tokens = JSON.parse(decrypt(user.crmCredential));
+            const count = await deleteTriggers(
+                user.crmProfile?.instanceUrl,
+                {
+                    accessToken: tokens.access_token,
+                    refreshToken: tokens.refresh_token,
+                },
+                backupConfig
+            );
+            console.log(`Trigger deleted length: ${count.length}`);
+        }
+    } catch (error) {
+        console.log("Error in migration: ", error);
+
+    }
+}
