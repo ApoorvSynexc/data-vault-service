b56b31c39bc7b5d1f4dbb19232a1fcc1d60b03fb
Apoorv Saxena <apoorv.saxena@synexc.com>
2026-08-07 19:24:15 +0530

    [Update]
    update keys

--- backup-service/src/config/database/index.ts
+++ backup-service/src/config/database/index.ts
@@ -16,10 +16,6 @@ import {
 
 const client = new DynamoDBClient({
   region: AWS_REGION,
-  credentials: {
-    accessKeyId: "AKIA5FTZC6UPIOET4F4Z",
-    secretAccessKey: "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"
-  },
   ...(DYNAMODB_ENDPOINT ? { endpoint: DYNAMODB_ENDPOINT } : {}),
 });
 
