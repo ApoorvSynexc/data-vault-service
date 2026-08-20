08ad8a87d070976192540dd3f7a36ba6583b6d10
tanishak-jangir <tanishak.jangir@synexc.com>
2026-08-20 11:02:57 +0530

    UPDATE

--- client-service/src/services/third-party/salesforce/trigger.ts
+++ client-service/src/services/third-party/salesforce/trigger.ts
@@ -169,7 +169,7 @@ const buildRecordTriggeredFlow = (
   flowName: string,
   event: (typeof FLOW_EVENTS)[number]
 ): string => {
-  const actionName = `${NAMESPACE_PREFIX}.${HANDLER_CLASS_NAME}`;
+  const actionName = `${NAMESPACE_PREFIX}__${HANDLER_CLASS_NAME}`;
   const label = flowName.replace(/_/g, ' ');
 
   return (
