f946b16408a7ca1d7eda4f84830254dbf5609384
Apoorv Saxena <apoorv.saxena@synexc.com>
2026-08-27 11:09:17 +0530

    [Add]
    update metadata handler

--- client-service/src/controller/v1/public/index.ts
+++ client-service/src/controller/v1/public/index.ts
@@ -188,7 +188,7 @@ const eventBridgeHandler = async (req: IRequest, res: IResponse): Promise<void> 
     await triggerBackupJob({ user, config, type: config.type === 'NORMAL' ? 'backup' : 'archival', lastUpdatedAt: config.lastBackupAt });
     makeResponse(req, res, 200, true, 'fetch');
   } catch (error) {
-    logger.error('realtime webhook processing error:', error);
+    logger.error('Event bridge trigger error: ', error);
   }
 };
 
