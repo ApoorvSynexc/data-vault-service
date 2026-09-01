155dbbcb035ed63290b625e1b2895e553b2a2fa4
Apoorv Saxena <apoorv.saxena@synexc.com>
2026-09-02 00:01:26 +0530

    [Update]
    restore code ingestion

--- client-service/src/services/third-party/salesforce/restore/field-creation.ts
+++ client-service/src/services/third-party/salesforce/restore/field-creation.ts
@@ -1,9 +1,9 @@
 import { logger } from "../../../../middlewares";
-import { IUser } from "../../../../models";
 import { getRestoreById } from "../../../restore";
 import { getRestoreJobById } from "../../../restore-job";
 import { getDecryptedCrmCredential, getUser } from "../../../user";
 import { ensureRestoreTrackingFields } from "../restore-fields";
+import { provisionRestorePermissionSet } from "../restore-permission-set";
 
 export const salesforceFieldCreation = async (params: { restoreJobId: string }) => {
     const { restoreJobId } = params;
@@ -51,5 +51,50 @@ export const salesforceFieldCreation = async (params: { restoreJobId: string }) 
         logger.info(`[salesforce-field-creation] restoreJobId=${restoreJobId} done`);
     } catch (error) {
         logger.error(`[salesforce-field-creation] restoreJobId=${restoreJobId} err:${error}`);
+    }
+}
+
+export const salesforceFieldsPermission = async (params: { restoreJobId: string }) => {
+    const { restoreJobId } = params;
+
+    logger.info(`[salesforce-fields-permission] restoreJobId=${restoreJobId} start`);
+    try {
+
+        const restoreJob = await getRestoreJobById(restoreJobId);
+        if (!restoreJob) {
+            return;
+        }
+
+        const restore = await getRestoreById(restoreJob.restoreId);
+        if (!restore) {
+            return;
+        }
+
+        const user = await getUser({ userId: restore.userId });
+        if (!user) {
+            return;
+        }
+
+        const objects = restoreJob.destination.objects;
+        if (user?.crmProfile?.instanceUrl) {
+            const tokens = getDecryptedCrmCredential(user);
+            const credentials = {
+                accessToken: tokens.access_token,
+                refreshToken: tokens.refresh_token,
+            }
+
+            const objectsNames = objects.map((object) => object.name);
+            await provisionRestorePermissionSet(
+                user?.crmProfile?.instanceUrl,
+                credentials,
+                user.crmProfile.userId,
+                objectsNames
+            );
+            logger.info(`[salesforce-fields-permission] restoreJobId=${restoreJobId} object=${objectsNames.length} done`);
+        }
+
+        logger.info(`[salesforce-fields-permission] restoreJobId=${restoreJobId} done`);
+    } catch (error) {
+        logger.error(`[salesforce-fields-permission] restoreJobId=${restoreJobId} err:${error}`);
     }
 }
